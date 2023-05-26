047c58653e5348ee43dc1872f8c98165dd58ec25
MasonLagrange <mason.lagrange@gmail.com>
2023-05-25 21:41:30 -0400

    fixed schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,7 +5,7 @@ const typeDefs = gql`
         _id: ID
         username: String
         email: String
-        bookCount: Integer
+        bookCount: Int
         savedBooks: [Book]!
     }
     type Book {
