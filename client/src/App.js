83ee7b9d2f3c32b051ca9b41dc2f265e10d5c6a6
MasonLagrange <mason.lagrange@gmail.com>
2023-05-29 23:24:27 -0400

    g

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 const httpLink = createHttpLink({
-  uri: process.env.MONGODB_URI
+  uri: '/graphql'
 })
 
 const authLink = setContext((_, { headers }) => {
