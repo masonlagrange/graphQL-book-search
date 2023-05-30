87fa29a82988281ceeee29010875842139888f1a
MasonLagrange <mason.lagrange@gmail.com>
2023-05-29 20:50:57 -0400

    finished bugfixing

--- server/server.js
+++ server/server.js
@@ -17,11 +17,16 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//app.use(routes);
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(_dirname, '../client/build/index.html'))
+})
 
 const startApolloServer = async () => {
   await server.start()
